08a728e8a93c26d972d018511cd1b59d18c41457
Tony Villa <tonyvilla77@gmail.com>
2020-12-25 10:56:48 -0800

    bd test

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://Tony:tony123@wjc-sog.wdkg9.mongodb.net/wjc-sog?retryWrites=true&w=majority",
+};
--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("this is a test.");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json);
+
+// DB config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
